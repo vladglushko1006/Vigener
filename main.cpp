beb67eebb24a71de9e966f7c30d0155db379d95e
vladglushko1006 <vladglushko1006@gmail.com>
2016-10-27 18:32:00 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+int a;
+scanf("%d", &a);
+if (a<1)
+	printf("NO");
+else 
+  {
+	if (a=1)
+	printf("YES");
+	else if (a%2==0)
+	a= a/2;
+	else 
+	printf("NO");
+  }
+ }
+
